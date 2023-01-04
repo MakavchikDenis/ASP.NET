196e5cec8cafd730b75f5ab51f98e6576f634d6f
Денис Макавчик <makavchik1.19@gmail.com>
2023-01-04 22:46:27 +0300

    Исправил игноры

---
+++ ClientProject/Models/Logs/InsertToLogs.cs
@@ -0,0 +1,12 @@
+﻿namespace ClientProject.Models.Logs
+{
+    public class InsertToLogs
+    {
+        // Исправить при написании авторизации
+        // На процедуре сейчас дефолтное значение ='new'
+        public string User { get; set; }
+        public string ActionType { get; set; }
+        public string ActionDetails { get; set; }
+        public string Details { get; set; }
+    }
+}
